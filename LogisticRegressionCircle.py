f087724869e3ac341ba7cf4ab95ed67653584a45
MomoCyann <541525127@qq.com>
2021-09-16 22:28:55 +0800

    9.16

--- LogisticRegressionCircle.py
+++ LogisticRegressionCircle.py
@@ -14,7 +14,7 @@ loss_save = []
 
 
 # 鸢尾花
-def load_data():
+def load_iris_data():
     iris = datasets.load_iris()
     x = iris['data']
     y = iris['target']
@@ -30,15 +30,49 @@ def load_data():
     return x_train,x_test,y_train,y_test
 
 
+# 圆数据集
+def load_circle_data():
+    dataframe = pd.read_csv("./data2.txt")
+    dataframe = dataframe.values
+    x = dataframe[:,:2]
+    y = dataframe[:,2:]
+    # 给x第一列加一列1，常数项
+    x_one = np.ones([len(x)])
+    x = np.insert(x,0,values=x_one,axis=1)
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=0)
+    return x_train,x_test,y_train,y_test
+
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def cal_hx_y():
+def cal_hx_y_linear():
     hx.clear()
     y.clear()
     for i in range(len(y_train)):
         hx.append(sigmoid(np.dot(theta,x_train[i])))
+        if hx[i] >= 0.5:
+            y.append(1)
+        else:
+            y.append(0)
+    return hx,y
+
+
+# 映射多项式
+def create_fx():
+    fx = np.empty([len(x_train),len(x_train[0])])
+    for i in range(len(x_train)):
+        for j in range(len(x_train[0])):
+            fx[i][j] = x_train[i][j] **2
+    return fx
+
+
+def cal_hx_y_more():
+    hx.clear()
+    y.clear()
+    for i in range(len(y_train)):
+        hx.append(sigmoid(np.dot(theta,fx[i])))
         if hx[i] >= 0.5:
             y.append(1)
         else:
@@ -65,7 +99,8 @@ def  gradiant_descent(theta):
 def predict():
     y_pred = np.empty([len(y_test)])
     for i in range(len(y_test)):
-        if sigmoid(np.dot(theta,x_test[i])) >= 0.5:
+        #if sigmoid(np.dot(theta,x_test[i])) >= 0.5:
+        if sigmoid(np.dot(theta, fx[i])) >= 0.5:
             y_pred[i] = 1
         else:
             y_pred[i] = 0
@@ -83,9 +118,10 @@ def cal_accuracy():
 
 if __name__ == "__main__":
     # 初始化数据集和参数和假设
-    x_train,x_test,y_train,y_test = load_data()
+    x_train,x_test,y_train,y_test = load_circle_data()
     theta = np.zeros([len(x_train[0])])
-    hx,y = cal_hx_y()
+    fx = create_fx()
+    hx,y = cal_hx_y_more()
     # 迭代
     for g in range(g):
         loss = cal_loss()
@@ -93,7 +129,7 @@ if __name__ == "__main__":
         if loss_min > loss:
             loss_min = loss
             theta = gradiant_descent(theta)
-            hx,y = cal_hx_y()
+            hx,y = cal_hx_y_more()
     print(loss_min)
     print(theta)
     y_pred = predict()
